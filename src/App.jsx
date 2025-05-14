821e65835e31b2d59c49d6b7e2bf60a48c60cbd3
KASAIJA KENNETH <kasaijak@gmail.com>
2025-05-14 14:23:58 +0300

    add home, navba and cart components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h3>NavBar</h3>
+    </div>
+  );
+};
+
+export default NavBar;
