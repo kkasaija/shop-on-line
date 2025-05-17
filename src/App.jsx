f9591e94f0f07814a62d238e162844739204c272
KASAIJA KENNETH <kasaijak@gmail.com>
2025-05-17 18:54:05 +0300

    style nav and not-found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Cart from './components/Cart';
+import NotFound from './components/NotFound';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
